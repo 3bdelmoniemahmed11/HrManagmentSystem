02c172c0b82f7eb8b7afaad85c86b205658bbc3e
3bdelmoniemahmed11 <3bdelmoniemahmed11@gmail.com>
2023-07-13 05:43:44 +0300

    Ayman/attendance Feature

--- HrManagment.BLL/Services/AttendanceServices/IAttendanceService.cs
+++ HrManagment.BLL/Services/AttendanceServices/IAttendanceService.cs
@@ -1,4 +1,5 @@
 ﻿using HrManagment.DAL.Models;
+using HrManagment.DAL.Models.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,17 @@ namespace HrManagment.BLL.Services.AttendanceService
 {
     public interface IAttendanceService
     {
-        Task<List<Attendance>> GetEmployeeAttendanceDays(int EmployeeId,DateTime startDate ,DateTime endDate);
+        Task<List<Attendance>> GetEmployeeAttendanceDays(int EmployeeId, DateTime startDate, DateTime endDate);
+        Task<List<EmpAttendanceDTO>> GetAllEmpsAttendances();
+        Task Delete(int id);
+        Task AddAttAsync(Attendance attendance);
+
+        Task<Attendance> GetByIdAsync(int id);
+        Task Update(Attendance attendance);
+        Task<Attendance> GetByIdAsynAsNoTracking(int id);
+        Task InsertListAsync(List<Attendance> list);
+
+
+
     }
 }
---
+++ HrMangment.DAL/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using HrManagment.DAL.Models;
+using HrManagment.DAL.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrManagment.DAL.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Attendance, EmpAttendanceDTO>()
+                .ForMember(dest => dest.EmpName, opt => opt.MapFrom(src => src.Employee.Name))
+                .ForMember(dest => dest.DeptName, opt => opt.MapFrom(src => src.Employee.Department.Name));
+
+            CreateMap<EmpAttendanceDTO, Attendance>()
+                   .ForMember(dest => dest.AttendanceTime, opt => opt.MapFrom(src => TimeSpan.Parse(src.AttendanceTime)))
+                .ForMember(dest => dest.DepartureTime, opt => opt.MapFrom(src => TimeSpan.Parse(src.DepartureTime)));
+
+        }
+    }
+    
+}
